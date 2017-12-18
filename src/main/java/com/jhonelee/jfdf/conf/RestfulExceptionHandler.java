b0575d69d7b9976f701afb6fdddeea0986e7dce4
lizhiqiang <13482477@qq.com>
2017-12-18 08:19:03 +0800

    commit

--- src/main/java/com/jhonelee/jfdf/conf/RestfulExceptionHandler.java
+++ src/main/java/com/jhonelee/jfdf/conf/RestfulExceptionHandler.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
